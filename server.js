a0cad564c01d79801c27ba2e9a0fafdf666b588a
David Aydin <david.aydin6@gmail.com>
2022-05-27 19:42:42 -0400

    Mongoose MongoDB setup

--- server.js
+++ server.js
@@ -5,4 +5,13 @@ const methodOverride = require("method-override");
 const PORT = 3000;
 const app = express();
 
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message + " is mongoDB not running?"));
+db.on("connected", () => console.log("Mongo Connected"));
+db.on("disconnected", () => console.log("Mongo Disconnected"));
+
 app.listen(PORT);
