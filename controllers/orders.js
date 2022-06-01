84a8c501239bd071ba8cf81021555396ad952596
David Aydin <david.aydin6@gmail.com>
2022-06-01 18:17:16 -0400

    Order Index

--- controllers/orders.js
+++ controllers/orders.js
@@ -2,10 +2,6 @@ const express = require("express");
 const orderRouter = express.Router();
 const Order = require("../models/order");
 
-// orderRouter.get("/", (req, res) => {
-//   res.send("hello");
-// });
-
 // Index
 orderRouter.get("/", (req, res) => {
   Order.find({}, (err, foundOrders) => {
