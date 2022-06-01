422aead7ba589ac72c20669a5d34d9c8594b974c
David Aydin <david.aydin6@gmail.com>
2022-06-01 17:37:46 -0400

    Started CRUD

--- controllers/orders.js
+++ controllers/orders.js
@@ -2,7 +2,24 @@ const express = require("express");
 const orderRouter = express.Router();
 const Order = require("../models/order");
 
-orderRouter.get("/", (req, res) => {
-  res.send("hello");
+// orderRouter.get("/", (req, res) => {
+//   res.send("hello");
+// });
+
+// Create
+orderRouter.post("/", (req,res) => {
+  Order.create(req.body, (err, createdOrder) => {
+    res.redirect("/order");
+  });
 });
+
+// Index
+orderRouter.get("/", (req,res) => {
+  Order.find({}, (err, foundOrders) => {
+    res.render("/order/index");
+  });
+});
+
+
+
 module.exports = orderRouter;
