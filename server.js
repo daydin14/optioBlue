c1a10a1b21a944698cc69d44ea997e5dfcd89378
David Aydin <david.aydin6@gmail.com>
2022-06-01 10:53:57 -0400

    Sign in with Email

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ db.on("error", (err) => console.log(err.message + " is mongoDB not running?"));
 db.on("connected", () => console.log("Mongo Connected"));
 db.on("disconnected", () => console.log("Mongo Disconnected"));
 
-app.use(methodOverride("_method"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
