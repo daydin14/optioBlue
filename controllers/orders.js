8e8c6ca46fd251c66bf87bbf565bf1c578ce9acf
David Aydin <david.aydin6@gmail.com>
2022-06-01 17:47:55 -0400

    New route

--- controllers/orders.js
+++ controllers/orders.js
@@ -6,20 +6,27 @@ const Order = require("../models/order");
 //   res.send("hello");
 // });
 
+// Index
+orderRouter.get("/", (req, res) => {
+  Order.find({}, (err, foundOrders) => {
+    res.render("/order/index", {
+      order: foundOrders,
+    });
+  });
+});
+// New
+orderRouter.get("/new", (req, res) => {
+  res.render("order/new.ejs");
+});
+// Delete
+// Update
 // Create
-orderRouter.post("/", (req,res) => {
+orderRouter.post("/", (req, res) => {
   Order.create(req.body, (err, createdOrder) => {
     res.redirect("/order");
   });
 });
-
-// Index
-orderRouter.get("/", (req,res) => {
-  Order.find({}, (err, foundOrders) => {
-    res.render("/order/index");
-  });
-});
-
-
+// Edit
+// Show
 
 module.exports = orderRouter;
