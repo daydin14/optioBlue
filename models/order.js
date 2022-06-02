5f0165385ac3fe383cf821bd12419d2b345de33e
David Aydin <david.aydin6@gmail.com>
2022-06-02 13:50:45 -0400

    Css fixes, model schema fix

--- models/order.js
+++ models/order.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const orderSchema = new Schema(
-  { clothing: String },
-  { qty: String },
-  { price: String },
-);
+const orderSchema = new Schema({
+   clothing: String,
+   qty: String,
+   price: String,
+});
 const Order = mongoose.model("Order", orderSchema);
 module.exports = Order;
